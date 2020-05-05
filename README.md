8902425dc1ddff4436770a37372507253ee53ee8
priyansi <priyanhiman@gmail.com>
2020-05-06 00:43:07 +0530

    added how to build max heap

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Algorithms in Python
+
 This repository will be updated regularly till I have coded all the algorithms taught in 6.006 Introduction To Algorithms by MIT.
--- max_heapify.py
+++ max_heapify.py
@@ -1,11 +1,23 @@
-def build_max_heap(arr):
-    pass
-
-
 def max_heapify(arr, node):
-    pass
+    def swap(arr, node):
+        temp = arr[node-1]
+        arr[node-1] = arr[2*node-1]
+        arr[2*node-1] = temp
+        return arr
+    if arr[node-1] < arr[2*node-1]:
+        arr = swap(arr, node)
+    if 2*node + 1 < len(arr):
+        if arr[node-1] < arr[2*node]:
+            arr = swap(arr, node)
+    return arr
+
+
+def build_max_heap(arr):
+    for node in range(len(arr)//2, 0, -1):
+        arr = max_heapify(arr, node)
+    return arr
 
 
 if __name__ == "__main__":
-    arr = [16, 4, 10, 14, 7, 9, 3, 2, 8, 1]
+    arr = [2, 4]
     print(build_max_heap(arr))
